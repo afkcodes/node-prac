c4e008398086f5d70c7549eaab5cf028d50fe17e
Ashish <ashish.kmr@outlook.com>
2020-07-28 01:53:45 +0530

    Done with Callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,33 +1,39 @@
 const got = require("got");
 
-const url =
-  "http://api.weatherapi.com/v1/current.json?key=a21aa72a43be4612a3072427202706&q=12.971599,77.594566";
-
-const mapBoxApi =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/bengal.json?access_token=pk.eyJ1IjoiYXNoaXNoZmVlbHMiLCJhIjoiY2tkM2hyajlzMWYwMTJxcXYybWltMHMzYSJ9.IBAdacbfAKd7HQKZ9gTkTg&limit=1";
-
-let lat, long;
-
-const makeRequest = async (reqUrl, type) => {
+const makeRequest = async (reqUrl, type, place, callback) => {
   try {
+    if (type === "map") {
+      reqUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?access_token=pk.eyJ1IjoiYXNoaXNoZmVlbHMiLCJhIjoiY2tkM2hyajlzMWYwMTJxcXYybWltMHMzYSJ9.IBAdacbfAKd7HQKZ9gTkTg&limit=1`;
+    }
     const response = await got(reqUrl);
     const body = JSON.parse(response.body);
     const data =
-      type === "weather" ?
-       body.current.temp_c :
-       body.features[0].center;
-    return data;
-  } catch (e) {
-    console.log(e);
+      type === "weather" ? body.current.temp_c : body.features[0].center;
+    if (data) {
+      callback(data, null);
+    }
+  } catch (err) {
+    if (err) {
+      callback(null, err);
+    }
   }
 };
 
-makeRequest(url, "weather").then((data) => {
-  console.log(`The Tempreture Outside is ${data} degree celcius`);
-});
+const tempreture = (data, error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`The Tempreture Outside is ${data} degree celcius`);
+  }
+};
 
-makeRequest(mapBoxApi, "map").then((data) => {
-  lat = data[1];
-  long = data[0];
-  console.log(`Latitude : ${lat} Longitude ${long}`);
-});
+const callback = (data, error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    url = `http://api.weatherapi.com/v1/current.json?key=a21aa72a43be4612a3072427202706&q=${data[1]},${data[0]} `;
+    makeRequest(url, "weather", null, tempreture);
+  }
+};
+
+makeRequest(null, "map", "ranchi", callback);
