88de7566bd6f7931b45333c8070cae53f0e20345
Ashish Kumar <ashish.kmr@outlook.com>
2020-08-05 01:53:55 +0530

    Mongo Crud finished

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,11 +1,13 @@
 // CRUD Operations
-require('dotenv').config()
-const mongodb = require("mongodb");
-const mongoClient = mongodb.MongoClient;
+require("dotenv").config();
+const { MongoClient, ObjectId } = require("mongodb");
 
 const databaseName = "task-manager";
 
-mongoClient.connect(
+// Generate New Id
+// const id = new ObjectId()
+
+MongoClient.connect(
   process.env.URL,
   { useUnifiedTopology: true },
   { useNewUrlParser: true },
@@ -16,17 +18,124 @@ mongoClient.connect(
     console.log("Connected");
 
     const db = client.db(databaseName);
-    db.collection("users").insertOne(
-      {
-        name: "Game",
-        age: 27,
-      },
-      (err, result) => {
-        if (err) {
-          console.log("somethhing went wrong unable to push data");
-        }
-        console.log(result.ops);
-      }
-    );
+
+    // InserOne -----------------------------------------------------------
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Ashish",
+    //     age: 25,
+    //   },
+    // (err, result) => {
+    //   if (err) {
+    //     console.log("somethhing went wrong unable to push data");
+    //   }
+    //   console.log(result.ops);
+    // }
+    // );
+
+    // InsertMany ---------------------------------------------------------
+
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       desc: "Learn Mongo",
+    //       status: "Ongoing",
+    //     },
+    //     {
+    //       name: "Create Api",
+    //       status: false,
+    //     },
+    //     {
+    //       name: "Integrate with Rb",
+    //       status: false,
+    //     },
+    //   ],
+    //   (err, result) => {
+    //     if (err) {
+    //       console.log("somethhing went wrong unable to push data");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // FindOne ------------------------------------------------------------
+    // db.collection("users").findOne({ name: "Ashish" }, (err, result) => {
+    //   if (err) {
+    //     return console.log("error Happened");
+    //   }
+
+    //   console.log(result);
+    // });
+
+    // Find ----------------------------------------------------------------
+    // db.collection("tasks").find({ status: false }).toArray((err, result) => {
+    //   if (err) {
+    //     return console.log("error Happened");
+    //   }
+
+    //   console.log(result);
+    // });
+
+    //  Update Set --------------------------------------------------------
+
+    // db.collection("users")
+    //   .updateOne(
+    //     { name: "Ashish" },
+    //     {
+    //       $set: { name: "Monchen" },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    //  Update Increment ---------------------------------------------------
+
+    // db.collection("users")
+    // .updateOne(
+    //   { name: "Monchen" },
+    //   {
+    //     $inc: { age: 1 },
+    //   }
+    // )
+    // .then((result) => {
+    //   console.log(result);
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+
+    //  UpdateMany -----------------------------------------------------
+
+    // db.collection("tasks")
+    //   .updateMany(
+    //     { status: false },
+    //     {
+    //       $set: {
+    //         status: true,
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    // DeleteMany ------------------------------------------------------
+    
+    // db.collection("tasks")
+    //   .deleteMany({ status: true })
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   }
 );
